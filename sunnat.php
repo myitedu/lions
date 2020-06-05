0485ea5d8c821f449ee31ec692e88f87600baebc
sunnat <sunnatnyc08gmail.com>
2020-06-04 22:06:34 -0400

    saving

--- sunnat.php
+++ sunnat.php
@@ -4,3 +4,7 @@ echo "<hr>";
 echo "I started to learn GIT";
 echo "<hr>";
 echo "i love programming";
+echo "<hr>";
+echo "I wanna be best software developer";
+echo "<hr>";
+echo "Today was june 4";
